0d10432a1fe2134d3c48ce7364fc83396655bd19
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-12 23:39:44 -0300

    resolvido pequeno erro

--- src/main/java/com/dev/hakeem/myfinanceapp/dto/TransferenciaDTO.java
+++ src/main/java/com/dev/hakeem/myfinanceapp/dto/TransferenciaDTO.java
@@ -1,7 +1,8 @@
 package com.dev.hakeem.myfinanceapp.dto;
 
 import com.dev.hakeem.myfinanceapp.entity.Conta;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,21 +10,26 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class TransferenciaDTO {
 
-    private  Long id;
-    @NotBlank
-    private double  valor;
-    @PastOrPresent(message = "A data deve ser o passado ou no presente")
-    private LocalDate data;
-    @NotBlank
-    private Conta origem;
-    @NotBlank
-    private  Conta destino;
-    private  Long contaId;
+    private Long id;
 
+    @NotNull(message = "O valor não pode ser nulo")
+    @Min(value = 0, message = "O valor deve ser maior que zero")
+    private Double valor;
+
+    @PastOrPresent(message = "A data deve ser no passado ou no presente")
+    private LocalDate data;
+
+    @NotNull(message = "A conta de origem não pode ser nula")
+    private Conta origem;
+
+    @NotNull(message = "A conta de destino não pode ser nula")
+    private Conta destino;
 }
+
--- src/main/java/com/dev/hakeem/myfinanceapp/service/TransferencaService.java
+++ src/main/java/com/dev/hakeem/myfinanceapp/service/TransferencaService.java
@@ -1,20 +1,22 @@
 package com.dev.hakeem.myfinanceapp.service;
 
+
 import com.dev.hakeem.myfinanceapp.dto.TransferenciaDTO;
 import com.dev.hakeem.myfinanceapp.entity.Conta;
 import com.dev.hakeem.myfinanceapp.repository.ContaRepository;
+import com.dev.hakeem.myfinanceapp.repository.TranferencaRepository;
 import com.dev.hakeem.myfinanceapp.repository.TranferencaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
 @Service
 public class TransferencaService {
-    @Autowired
-    private  final TranferencaRepository repository;
-    @Autowired
+    private final TranferencaRepository repository;
     private final ContaRepository contaRepository;
 
+    @Autowired
     public TransferencaService(TranferencaRepository repository, ContaRepository contaRepository) {
         this.repository = repository;
         this.contaRepository = contaRepository;
@@ -26,46 +28,41 @@ public class TransferencaService {
      * @param transferenciaDTO Objeto contendo as informações da transferência.
      * @throws IllegalArgumentException se as contas de origem ou destino forem nulas ou o valor da transferência for não positivo.
      */
-
-    public  void transferencia(TransferenciaDTO transferenciaDTO){
+    public void transferir(TransferenciaDTO transferenciaDTO) {
         // Verifica se a origem e destino são válidos
-        if (transferenciaDTO.getOrigem() == null || transferenciaDTO.getDestino() == null){
-            throw  new IllegalArgumentException("Conta origem e destino devem ser especificadas.");
+        if (transferenciaDTO.getOrigem() == null || transferenciaDTO.getDestino() == null) {
+            throw new IllegalArgumentException("Conta origem e destino devem ser especificadas.");
         }
 
         // Verifica se o valor da transferência é positivo
-        if (transferenciaDTO.getValor() <= 0){
-            throw  new IllegalArgumentException("O valor da transferência deve ser positivo.");
-
+        if (transferenciaDTO.getValor() <= 0) {
+            throw new IllegalArgumentException("O valor da transferência deve ser positivo.");
         }
 
         // Busca as contas de origem e destino no banco de dados
-        Optional<Conta> contaOrigem1 = contaRepository.findById(transferenciaDTO.getOrigem().getId());
-        Optional<Conta> contaDestino1 = contaRepository.findById(transferenciaDTO.getDestino().getId());
+        Optional<Conta> contaOrigemOpt = contaRepository.findById(transferenciaDTO.getOrigem().getId());
+        Optional<Conta> contaDestinoOpt = contaRepository.findById(transferenciaDTO.getDestino().getId());
 
         // Verifica se as contas existem
-        if (!contaOrigem1.isPresent() || !contaDestino1.isPresent()){
+        if (!contaOrigemOpt.isPresent() || !contaDestinoOpt.isPresent()) {
             throw new IllegalArgumentException("Conta origem ou destino não encontrada.");
         }
 
-        Conta contaOrigem = contaOrigem1.get();
-        Conta contaDestino = contaDestino1.get();
+        Conta contaOrigem = contaOrigemOpt.get();
+        Conta contaDestino = contaDestinoOpt.get();
 
         // Realiza a lógica de débito e crédito nas contas
-        debitar(contaOrigem,transferenciaDTO.getValor());
-        creditar(contaDestino,transferenciaDTO.getValor());
+        debitar(contaOrigem, transferenciaDTO.getValor());
+        creditar(contaDestino, transferenciaDTO.getValor());
 
         // Salva as alterações nas contas
         contaRepository.save(contaOrigem);
         contaRepository.save(contaDestino);
 
-
         // Simulação da transferência com sucesso
         System.out.println("Transferência realizada com sucesso! Transferido " + transferenciaDTO.getValor() +
                 " de " + contaOrigem.getUser().getName() + " para " + contaDestino.getUser().getName() +
                 " na data " + transferenciaDTO.getData());
-
-
     }
 
 
@@ -73,33 +70,28 @@ public class TransferencaService {
      * Debita um valor da conta de origem.
      *
      * @param origem A conta de onde o valor será debitado.
-     * @param valor O valor a ser debitado.
+     * @param valor  O valor a ser debitado.
      * @throws IllegalArgumentException se o saldo da conta de origem for insuficiente.
      */
-
-    public void debitar(Conta origem,double valor){
+    private void debitar(Conta origem, double valor) {
         // Verifica se a conta de origem tem saldo suficiente
-        if (origem.getSaldoInicial()< valor){
-            throw  new IllegalArgumentException("Saldo insuficiente na conta de origem.");
+        if (origem.getSaldoInicial() < valor) {
+            throw new IllegalArgumentException("Saldo insuficiente na conta de origem.");
         }
 
         // Debita o valor da conta de origem
-        origem.setSaldoInicial(origem.getSaldoInicial()- valor);
+        origem.setSaldoInicial(origem.getSaldoInicial() - valor);
     }
 
     /**
      * Credita um valor na conta de destino.
      *
      * @param destino A conta onde o valor será creditado.
-     * @param valor O valor a ser creditado.
+     * @param valor   O valor a ser creditado.
      */
-
-    public void creditar(Conta destino, double valor){
+    private void creditar(Conta destino, double valor) {
         // Credita o valor na conta de destino
         destino.setSaldoInicial(destino.getSaldoInicial() + valor);
     }
-
-
-
-
 }
+
