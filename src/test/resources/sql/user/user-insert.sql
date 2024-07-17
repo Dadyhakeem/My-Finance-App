287366f263cccf4a739713c34ed79ad33b98a58f
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-16 21:32:33 -0300

    configurando o ambiente de testes

---
+++ src/test/resources/sql/user/user-insert.sql
@@ -0,0 +1,3 @@
+insert into User (id,name,email , senha,role)values(100, "dady","dady@gmail.com","123456","ROLR_ADMIN");
+insert into User (id,name,email , senha,role)values(101, "taizy","taizy@gmail.com","123456","ROLR_CLIENT");
+insert into User (id,name,email , senha,role)values(102, "ciro","ciro@gmail.com","123456","ROLE_CLIENT");
